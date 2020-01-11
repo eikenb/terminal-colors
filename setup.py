f4e392f102f1fa653412e72a9c25dfbfad4f57d8
John Eikenberry <jae@zhar.net>
2020-01-10 18:24:04 -0800

    language classifier only includes major versions

--- setup.py
+++ setup.py
@@ -27,8 +27,6 @@ setup(name='terminal-colors',
         'Topic :: Terminals',
         'Topic :: Utilities',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
     ],
 )
